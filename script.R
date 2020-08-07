0aaf43383ca821fbcfd09e3fe473d2164481044d
Pieter Provoost <pieterprovoost@gmail.com>
2020-08-07 16:08:40 +0200

    crs fix

--- script.R
+++ script.R
@@ -7,13 +7,19 @@ library(ggplot2)
 library(lwgeom)
 library(maptools)
 library(rnaturalearth)
+library(nngeo)
 
 # fetch occurrences
 
 df <- occurrence("Porifera", qcfields = TRUE, dropped = "include")
 
 df_sf <- df %>%
-  st_as_sf(coords = c("decimalLongitude", "decimalLatitude"), crs = 4326, remove = FALSE)
+  filter(!is.na(decimalLongitude) & !is.na(decimalLatitude)) %>%
+  select(id, decimalLongitude, decimalLatitude) %>%
+  st_as_sf(coords = c("decimalLongitude", "decimalLatitude"), crs = 4326)
+
+ggplot() +
+  geom_sf(data = df_sf)
 
 # arctic
 
@@ -28,8 +34,8 @@ amap <- st_read("AMAP-area/amaplim_lam_poly.shp") %>%
 arctic <- st_union(aba, amap)
 
 ggplot() +
-  geom_sf(data = aba, fill = "green", color = "green", alpha = 0.1) +
-  geom_sf(data = amap, fill = "red", color = "red", alpha = 0.1) +
+  geom_sf(data = aba, fill = "skyblue3", color = "chartreuse3", alpha = 0.1) +
+  geom_sf(data = amap, fill = "coral3", color = "coral3", alpha = 0.1) +
   geom_sf(data = arctic, fill = NA, color = "black")
 
 # antarctic
@@ -39,31 +45,26 @@ asds <- load_ASDs()
 antarctic <- asds %>%
   st_as_sf() %>%
   st_union() %>%
-  lapply(FUN = function(x) x[1]) %>%
-  st_multipolygon() %>%
-  st_sfc(crs = st_crs(arctic))
-  
+  st_remove_holes()
+
 ggplot() +
-  geom_sf(data = antarctic, fill = "green", color = "green", alpha = 0.1)
+  geom_sf(data = antarctic, fill = NA, color = "black")
 
 # match geometries
 
 df_sf$is_arctic <- as.logical(st_intersects(df_sf %>% st_transform(crs = st_crs(arctic)), arctic))
 df_sf$is_antarctic <- as.logical(st_intersects(df_sf %>% st_transform(crs = st_crs(antarctic)), antarctic))
 
-# plot
-
 ggplot() +
   geom_sf(data = ne_countries(returnclass = "sf"), color = "#000000", fill = "#eeeeee") +
-  geom_sf(data = df_sf, size = 0.5) +
-  geom_sf(data = df_sf %>% filter(is_arctic), color = "coral2") +
+  geom_sf(data = df_sf %>% filter(is_arctic), color = "skyblue3") +
   geom_sf(data = df_sf %>% filter(is_antarctic), color = "coral3") +
   coord_sf(crs = 4326) +
   theme_void()
 
 ggplot() +
   geom_sf(data = arctic, fill = "black", color = "black", alpha = 0.05) +
-  geom_sf(data = df_sf %>% filter(is_arctic), color = "coral2") +
+  geom_sf(data = df_sf %>% filter(is_arctic), color = "skyblue3") +
   coord_sf(crs = st_crs(arctic))
 
 ggplot() +
@@ -71,11 +72,22 @@ ggplot() +
   geom_sf(data = df_sf %>% filter(is_antarctic), color = "coral3") +
   coord_sf(crs = st_crs(antarctic))
 
+# add to original dataframe
+
+df_joined <- df %>%
+  left_join(df_sf, by = "id")
+
 # output
 
-df_selection <- df_sf %>%
-  as.data.frame() %>%
+df_selection <- df_joined %>%
   select(id, dataset_id, institutionCode, collectionCode, catalogNumber, recordNumber, fieldNumber, locality, decimalLongitude, decimalLatitude, date_year, date_mid, eventDate, scientificName, originalScientificName, aphiaID, minimumDepthInMeters, maximumDepthInMeters, depth, coordinateUncertaintyInMeters, phylum, class, subclass, order, suborder, family, subfamily, genus, subgenus, species, subspecies, phylumid, classid, subclassid, orderid, suborderid, familyid, subfamilyid, genusid, subgenusid, speciesid, subspeciesid, flags, shoredistance, bathymetry, is_arctic, is_antarctic)
+
+ggplot() +
+  geom_sf(data = ne_countries(returnclass = "sf"), color = "#000000", fill = "#eeeeee") +
+  geom_point(data = df_selection %>% filter(is_arctic), aes(decimalLongitude, decimalLatitude), color = "skyblue3") +
+  geom_point(data = df_selection %>% filter(is_antarctic), aes(decimalLongitude, decimalLatitude), color = "coral3") +
+  coord_sf(crs = 4326) +
+  theme_void()
 
 write.csv(df_selection, file = paste0("porifera_", format(Sys.time(), "%Y%m%d"), ".csv"), row.names = FALSE, na = "")
 
